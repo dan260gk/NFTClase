ee5255f9d89d60573ce73fe2fb4a1267edc48146
DAN <jebusesquivel@outlook.com>
2024-04-24 07:45:46 -0500

    ntfs.js

--- scripts/ntfs.js
+++ scripts/ntfs.js
@@ -13,3 +13,28 @@ const {
     PUBLIC_KEY,
     CONTRACT_ADDRESS
 } = process.env
+
+async function createImgInfo(imageRoute){
+    const authResponse = await axios.get('https://api.pinata.cloud/data/testAuthentication',{
+        headers:{
+            pinata_api_key:PINATA_API_KEY,
+            pinata_secret_api_key:PINATA_SECRET_KEY
+        }
+    })
+    console.log(authResponse)
+    const stream = fs.createReadStream(imageRoute)
+    const data = new FormData()
+    data.append("file",stream)
+    const fileResponse = await axios.post('https://api.pinata.cloud/pinning/pinFileToIPFS',data,{
+        headers:{
+            "Content-type": `multipart/forma-data: boundary=${data._boundary}`,
+            pinata_api_key:PINATA_API_KEY,
+            pinata_secret_api_key:PINATA_SECRET_KEY
+        }
+    })
+    const {data: fileData={}}=fileResponse
+    const{IpfsHash}=fileData
+    const fileIPFS = `https://gateway.pinata.cloud/ipfs/${IpfsHash}`
+    return fileIPFS
+
+}
